3ce0817254dc635c39b8f479ab43f7606ca9291c
had3sgames <had3sgames@gmail.com>
2026-09-15 13:37:13 +0000

    Extend src/circuit_breaker.rs (part 3/17)

--- src/circuit_breaker.rs
+++ src/circuit_breaker.rs
@@ -2,3 +2,10 @@ use std::sync::atomic::{AtomicU32, AtomicU64, Ordering};
 use std::time::{Duration, SystemTime, UNIX_EPOCH};
 
 use crate::error::AppError;
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum State {
+    Closed,
+    Open,
+    HalfOpen,
+}
