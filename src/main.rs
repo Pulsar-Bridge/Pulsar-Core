92e3118040e201b644a267af2fee4c9fba96d490
had3sgames <had3sgames@gmail.com>
2026-09-15 13:37:18 +0000

    Extend src/main.rs (part 5/19)

--- src/main.rs
+++ src/main.rs
@@ -22,3 +22,12 @@ use crate::contracts::SorobanContractClient;
 use crate::horizon::HorizonClient;
 use crate::redis_store::IdempotencyStore;
 use crate::state::AppState;
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    tracing_subscriber::fmt()
+        .with_env_filter(
+            tracing_subscriber::EnvFilter::try_from_default_env().unwrap_or_else(|_| "info".into()),
+        )
+        .json()
+        .init();
