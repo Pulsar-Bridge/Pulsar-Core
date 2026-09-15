818afac5af22fe26a1b0c9c753117e79e8ac05c9
had3sgames <had3sgames@gmail.com>
2026-09-15 13:37:17 +0000

    Extend src/handlers/admin.rs (part 3/5)

--- src/handlers/admin.rs
+++ src/handlers/admin.rs
@@ -6,3 +6,9 @@ use uuid::Uuid;
 use crate::db::{self, transactions};
 use crate::error::AppResult;
 use crate::state::AppState;
+
+#[derive(Deserialize)]
+pub struct ListQuery {
+    #[serde(default = "default_limit")]
+    limit: i64,
+}
