82023d35043f678b06fe841d1b7667265a39fdf4
had3sgames <had3sgames@gmail.com>
2026-09-15 13:37:13 +0000

    Extend src/redis_store.rs (part 4/11)

--- src/redis_store.rs
+++ src/redis_store.rs
@@ -4,3 +4,9 @@ use redis::aio::ConnectionManager;
 use redis::AsyncCommands;
 
 use crate::error::{AppError, AppResult};
+
+#[derive(Clone)]
+pub struct IdempotencyStore {
+    conn: ConnectionManager,
+    ttl: Duration,
+}
