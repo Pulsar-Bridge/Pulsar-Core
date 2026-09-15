1f75626aadaa0fcf5b0c1e6274df29b92bce2259
had3sgames <had3sgames@gmail.com>
2026-09-15 13:37:17 +0000

    Add imports to src/handlers/health.rs

--- src/handlers/health.rs
+++ src/handlers/health.rs
@@ -3,3 +3,6 @@ use axum::http::StatusCode;
 use axum::response::IntoResponse;
 use axum::Json;
 use serde_json::json;
+
+use crate::circuit_breaker::State as BreakerState;
+use crate::state::AppState;
