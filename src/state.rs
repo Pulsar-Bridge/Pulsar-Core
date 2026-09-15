a1cac6bd1fbefd3567c28f20d1d6237c19586d5a
had3sgames <had3sgames@gmail.com>
2026-09-15 13:37:15 +0000

    Extend src/state.rs (part 4/8)

--- src/state.rs
+++ src/state.rs
@@ -9,3 +9,9 @@ use uuid::Uuid;
 use crate::contracts::ContractClient;
 use crate::horizon::HorizonClient;
 use crate::redis_store::IdempotencyStore;
+
+pub type KeyedRateLimiter = RateLimiter<
+    String,
+    governor::state::keyed::DefaultKeyedStateStore<String>,
+    governor::clock::DefaultClock,
+>;
